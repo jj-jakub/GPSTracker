c43cee01865c24c0617235f8ec3da849896849ba
Jakub Jasiński <jj.jakub.jasinski@gmail.com>
2024-07-10 15:44:16 +0200

    Change colors

--- design/src/main/java/com/jj/gpstracker/design/BaseColors.kt
+++ design/src/main/java/com/jj/gpstracker/design/BaseColors.kt
@@ -2,7 +2,7 @@ package com.jj.gpstracker.design
 
 import androidx.compose.ui.graphics.Color
 
-val colorPrimary = Color(0xFFFF9800)
+val colorPrimary = Color(0xFF3F51B5)
 val colorPrimaryDark = Color(0xFF474747)
 val colorAccent = Color(0xFFA7A7A7)
 
